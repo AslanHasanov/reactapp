010030ad549ef4487d47e501925afc0ab94f0117
AslanHasanov <aslanrh@code.edu.az>
2023-04-25 02:02:55 +0400

    Header Footer components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react'
-import UserList from './components/UserList'
+import Header from './components/Header/Header'
 
  class App extends Component {
-  state={
-    isValid:true,
-    users:[
-      {id:1,name:"Aslan"},
-      {id:2,name:"Elnur"},
-      {id:3,name:"Qember"}
-    ]
-  }
-
-  DeleteUser=(id)=>{
-    this.setState({
-      users:this.state.users.filter(x=>x.id !==id)
-    })
-  }
 
   render() {
     return (
       <div>     
-        <h3 style={this.state.users.length===0?{"color":"red"}: {"color":"blue"}}>Istifadeciler</h3> 
-        {
-          this.state.users.length === 0 ? "istifadeci movcud deyil" : 
-          <UserList DeleteUser={this.DeleteUser} users={this.state.users}/>
-        }
+      <Header />
       </div>
     )
   }
